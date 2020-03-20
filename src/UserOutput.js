2aa2da7d46a9061d6b7ea9ad51369f885b3aa77f
Gordon Yu <gyunit@gmail.com>
2020-03-19 23:01:46 -0700

    userInput and userOutput functional components declared

---
+++ src/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>P1</p>
+            <p>P2</p>
+        </div>
+    );
+};
+
+export default userOutput;
